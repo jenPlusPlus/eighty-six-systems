b576e8ea81c4c27286b90d52ad67b8b5707feca1
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-28 20:45:15 -0600

    add PropTypes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const Header = (props) => {
   console.log('header props: ', props);
   return (
     <div className="header">
       <h1>eighty-six</h1>
+      { !props.currentUser.loginCode &&
+        <Link to={'/login'}>
+          <button className="login-button">Login</button>
+        </Link>
+      }
+      { props.currentUser.loginCode &&
+        <Link to={'/'}>
+          <button className="logout-button">Logout</button>
+        </Link>
+      }
     </div>
   );
+};
+
+Header.propTypes = {
+  currentUser: PropTypes.object
 };
 
 export default Header;
