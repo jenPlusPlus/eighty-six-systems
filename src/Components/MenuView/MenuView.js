51f6e301617ebf469445bb480c4a47b812b30d5c
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-01 16:12:19 -0600

    Redirect to login page from any other page when not logged in

--- src/Components/MenuView/MenuView.js
+++ src/Components/MenuView/MenuView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 class MenuView extends Component {
   constructor() {
@@ -8,19 +8,26 @@ class MenuView extends Component {
   }
   render() {
     return (
-      <div className='menu-view'>
-        <div className='go-back'>
-          <Link to={`/${this.props.currentUser.loginCode}/tables`}>
-            <h3 className='all-tables-button'>All Tables</h3>
-          </Link>
-          <Link to={`/${this.props.currentUser.loginCode}/tables/${this.props.currentTable.tableNumber}`}>
-            <h3 className='table-info-menuview table-button'>Table {this.props.currentTable.tableNumber}</h3>
-          </Link>
-          <h3 className='table-info-menuview seat-button'>Seat {this.props.currentSeat.seatNumber}</h3>
+      <div className='menu-view-wrapper'>
+        {this.props.currentUser.loginCode &&
+          <div className="menu-view">
+            <div className='go-back'>
+              <Link to={`/${this.props.currentUser.loginCode}/tables`}>
+                <h3 className='all-tables-button'>All Tables</h3>
+              </Link>
+              <Link to={`/${this.props.currentUser.loginCode}/tables/${this.props.currentTable.tableNumber}`}>
+                <h3 className='table-info-menuview table-button'>Table {this.props.currentTable.tableNumber}</h3>
+              </Link>
+              <h3 className='table-info-menuview seat-button'>Seat {this.props.currentSeat.seatNumber}</h3>
+            </div>
+            <div className='menu'>
+              <h3>MENU VIEW</h3>
+            </div>
           </div>
-        <div className='menu'>
-          <h3>MENU VIEW</h3>
-        </div>
+        }
+        {!this.props.currentUser.loginCode &&
+          <Redirect to={'/login'} />
+        }
       </div>
     );
   }
