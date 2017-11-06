fe83c9e30233f0b3f64267399fbbbe22fc0c8e10
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-05 18:45:40 -0700

    Style All Seats and Seat X buttons

--- src/Components/SeatManager/SeatManager.js
+++ src/Components/SeatManager/SeatManager.js
@@ -71,6 +71,9 @@ class SeatManager extends Component {
       <div className="server-dashboard-wrapper">
         {this.props.currentUser.loginCode &&
           <div className='server-dashboard'>
+            <Link to={`/${this.props.currentUser.loginCode}/tables`}>
+              <h3 className='all-tables-button'>All Tables</h3>
+            </Link>
             <form>
               <input type='text'
                 placeholder='Enter Seat Number'
@@ -79,9 +82,6 @@ class SeatManager extends Component {
               <button className='add-seat'
                 onClick={(event) => this.handleSubmit(event)}>Add Seat</button>
             </form>
-            <Link to={`/${this.props.currentUser.loginCode}/tables`}>
-              <h3 className='all-tables-button'>All Tables</h3>
-            </Link>
             <h3 className='table-info-seat-manager'>Table {this.props.currentTable.tableNumber}</h3>
             <div className='seat-container'>
               {this.mapSeats()}
