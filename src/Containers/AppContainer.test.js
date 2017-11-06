3af31a73afa1690cc432262e8751bf25e507bf14
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-11-06 08:49:02 -0700

    Add App Container test

---
+++ src/Containers/AppContainer.test.js
@@ -0,0 +1,43 @@
+import configureStore from 'redux-mock-store';
+import { shallow, mount } from 'enzyme';
+import { connect } from 'react-redux';
+import AppContainer from './AppContainer';
+import App from './../Components/App/App';
+import React from 'react';
+import * as actions from './../Actions/index';
+
+describe('AppContainer tests', () => {
+
+  it('container should have default state', () => {
+
+    const mockStore = configureStore()
+    const initialState = { currentUser: {} }
+    const store = mockStore(initialState)
+    const wrapper = shallow(
+        <App
+          store={store}
+          currentUser={initialState}
+        />
+        )
+
+
+    expect(wrapper.instance().props.currentUser).toEqual({ currentUser: {} });
+  });
+
+  it('container should add a user to state', () => {
+
+    const mockStore = configureStore()
+    const initialState = { currentUser: {name: 'Jen', loginCode: '0000'} }
+    const store = mockStore(initialState)
+    const wrapper = shallow(
+        <App
+          store={store}
+          currentUser={initialState}
+        />
+        )
+
+
+    expect(wrapper.instance().props.currentUser).toEqual({ currentUser: {name: 'Jen', loginCode: '0000'} });
+  });
+
+});
